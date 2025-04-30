119b708786f91911c783fc4badf4fc711a7c71aa
Liry24 <47878070+Liry24@users.noreply.github.com>
2025-04-30 15:22:29 +0900

    fix: update vrchat URL in redirects and correct booth link in index

--- src/utils/redirects.ts
+++ src/utils/redirects.ts
@@ -6,6 +6,7 @@ const redirects = {
     x: 'https://x.com/Liry24g',
     twitter: 'https://twitter.com/Liry24g',
     discord: 'https://discord.com/users/365789612024659969',
+    vrchat: 'https://vrchat.com/home/user/usr_933f7024-bf54-408e-88fe-43bf2b81ccf8',
     avatio: 'https://avatio.me/@aed3763b-0e15-46e1-b44d-b9a0d5390332',
     spotify: 'https://spti.fi/liry24',
     steam: 'https://steamcommunity.com/id/Liry24',
