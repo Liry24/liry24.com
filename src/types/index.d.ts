e773268c2ccd2b92bfc14e629a4688f08122b366
Liry24 <liry241999@gmail.com>
2025-05-03 19:13:13 +0900

    feat: add new components for arts, posts, and Liria sections; integrate ofetch for dynamic content chore: update package dependencies and add type definitions for Zenn API style: enhance layout with improved responsiveness and structure

---
+++ src/types/index.d.ts
@@ -0,0 +1,37 @@
+interface zenn {
+    articles: {
+        id: number
+        post_type: 'Article'
+        title: string
+        slug: string
+        comments_count: number
+        liked_count: number
+        bookmarked_count: number
+        body_letters_count: number
+        article_type: 'idea' | 'tech'
+        emoji: string
+        is_suspending_private: boolean
+        published_at: string
+        body_updated_at: string
+        source_repo_updated_at: string
+        pinned: boolean
+        path: string
+        user: {
+            id: number
+            username: string
+            name: string
+            avatar_small_url: string
+        }
+        publication: {
+            id: number
+            name: string
+            display_name: string
+            avatar_small_url: string
+            avatar_url: string
+            pro: boolean
+            avatar_registered: boolean
+        }
+    }[]
+    next_page: number | null
+    total_count: number | null
+}
