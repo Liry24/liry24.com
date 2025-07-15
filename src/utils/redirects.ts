312bb4f12278b75357044102f73eb4e69e1c3ad5
Liry24 <47878070+Liry24@users.noreply.github.com>
2025-07-15 17:04:25 +0900

    feat: update avatio redirect link to point to the correct user profile

--- src/utils/redirects.ts
+++ src/utils/redirects.ts
@@ -7,7 +7,7 @@ const redirects = {
     twitter: 'https://twitter.com/Liry24g',
     discord: 'https://discord.com/users/365789612024659969',
     vrchat: 'https://vrchat.com/home/user/usr_933f7024-bf54-408e-88fe-43bf2b81ccf8',
-    avatio: 'https://avatio.me/@aed3763b-0e15-46e1-b44d-b9a0d5390332',
+    avatio: 'https://avatio.me/@liry24',
     spotify: 'https://spti.fi/liry24',
     steam: 'https://steamcommunity.com/id/Liry24',
     valorant: 'https://tracker.gg/valorant/profile/riot/Liry%23yoshi/overview',
