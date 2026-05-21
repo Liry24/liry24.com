acc7fd963648c9f2c95d8475337228ef13c82b87
Liry24 <liry241999@gmail.com>
2026-05-21 12:43:57 +0900

    fix: simplify unauthorized error handling in purge-cache API

--- server/api/purge-cache.post.ts
+++ server/api/purge-cache.post.ts
@@ -2,11 +2,7 @@ export default eventHandler(async (event) => {
     const runtimeConfig = useRuntimeConfig()
     const body = await readBody(event)
 
-    if (body.token !== runtimeConfig.bypassToken)
-        throw createError({
-            statusCode: 401,
-            statusMessage: 'Unauthorized',
-        })
+    if (body.token !== runtimeConfig.bypassToken) throw serverError.unauthorized()
 
     await useStorage('cache').remove('nitro:functions:getSocials:default.json')
 
