048318d205229d48b32d8c46da32c5c706c2fd94
Liry24 <liry241999@gmail.com>
2026-05-21 12:43:12 +0900

    fix: remove revalidateISR function and simplify purgeRuntimeCache API call

--- server/utils/cache.ts
+++ server/utils/cache.ts
@@ -1,21 +1,8 @@
-export const revalidateISR = async () => {
-    const runtimeConfig = useRuntimeConfig()
-    const token = runtimeConfig.bypassToken as string | undefined
-
-    if (!import.meta.dev && token) {
-        await $fetch(runtimeConfig.public.homeDomain as string, {
-            headers: {
-                'x-prerender-revalidate': token,
-            },
-        })
-    }
-}
-
 export const purgeRuntimeCache = async () => {
     const runtimeConfig = useRuntimeConfig()
 
     if (!import.meta.dev)
-        await $fetch(`${runtimeConfig.public.homeDomain as string}/api/purge-cache`, {
+        await $fetch('/api/purge-cache', {
             method: 'POST',
             body: {
                 token: runtimeConfig.bypassToken,
